eea659cd7d30a7b42a486ad8714f1cf6ae04fd5e
Nick Dickson <nad35@byu.edu>
2018-12-03 21:42:06 -0700

    Fixed Audio Issue. Adjusted Buttons.

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(6);
+    }
+}
