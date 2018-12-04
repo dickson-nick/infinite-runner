16574b2840a37e0c3fb7dc7621eff6f390bbf7ed
Nick Dickson <nad35@byu.edu>
2018-12-03 19:34:17 -0700

    Revamped Main Menu and cleaned up some code

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = transform.position + new Vector3(0.0f, 0.0f, 1.0f);
+
+        if (transform.position.z >= 4000) {
+            transform.position = new Vector3(-16.0f, -11.6f, 48.4f);
+        }
+	}
+}
