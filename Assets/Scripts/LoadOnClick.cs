81243efe44a6603f75ea5446acbe5b8e8620da35
Nick Dickson <nad35@byu.edu>
2018-12-05 16:41:34 -0700

    Added Quit Button (Does not work in Unity test. Only works if game is built and run.

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -7,15 +7,18 @@ public class LoadOnClick : MonoBehaviour {
 
     public void LoadScene(int level)
     {
-        if (level != -1)
-        {
-            SceneManager.LoadScene(level);
-        }
-        else
+        if (level == -1)
         {
             PlayerPrefs.SetInt("UnlockedLevel", 1);
 
             GameObject.Find("Canvas").GetComponent<UnlockLevels>().Invoke("Awake", 0);
+        } else if (level == -2)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(level);
         }
     }
 }
